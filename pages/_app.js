a7486718b0d7cdab38766edcc7ea6caebc39cf2e
ttmone <tomone.ameno@gmail.com>
2021-07-26 08:39:02 +0900

    Google AnalyticsでPV計測できるように実装

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
 import '../styles/global.css'
 import 'highlight.js/styles/base16/darcula.css'
+import * as gtag from '../lib/gtag'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-export default function App({ Component, pageProps}) {
+const App = ({ Component, pageProps}) => {
+  const router = useRouter()
+  useEffect(() => {
+    // if (!gtag.exitsGaId) {
+    //   return
+    // }
+
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
+
+export default App
